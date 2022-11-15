60268335086bc0ac703aada0eb30b7f2ff43f845
Shadin Al-Arab <shadin.alarab@gmail.com>
2022-11-15 10:43:11 -0800

    window event listener added

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,7 +2,16 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
+window.addEventListener('beforeinstallprompt', (event) => { 
+    console.log('beforeinstallprompt','beforeinstallprompt', event);
+    //PREVENT DEFAULT
+    event.preventDefault();
+    // STASH THE EVENT SO WE CAN TRIGGER IT LATER
+    deferredPrompt = event;
+    // REMOVE THE HIDDEN CLASS FROM THE INSTALL CONTAINER
+    divInstall.classList.toggle('hidden',false);
+    });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
